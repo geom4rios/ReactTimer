4c08040031a8f0f27fe459d9d8bc82d809530bd4
geom4rios <geom4rios@gmail.com>
2017-06-13 21:42:34 +0300

    ReactTimer first commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+
+var Navigation = require('Navigation');
+
+var Main = (props) => {
+  return (
+    <div>
+      <div>
+      <Navigation/>
+        <div>
+          <p>Main.jsx Rendered</p>
+          {props.children}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+module.exports = Main;
