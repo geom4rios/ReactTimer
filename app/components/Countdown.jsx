80b845a3928cf7e107fcf8a799ae96d4400f7fce
geom4rios <geom4rios@gmail.com>
2017-06-20 21:49:18 +0300

    update status to stop when count reaches zero

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -27,12 +27,29 @@ var Countdown = React.createClass({
             }
         }
     },
+    componentWillUpdate: function (nextProps, nextState) {
+
+    },
+    componentWillUnmount: function () {
+        console.log('Component did unmount');
+    },
+    componentWillMount: function () {
+      console.log('Component will mount');
+      clearInterval(this.timer);
+      this.timer = undefined;
+    },
+    componentDidMount: function () {
+      console.log('componentDidMount');
+    },
     startTimer: function () {
       this.timer = setInterval(() => {
           var newCount = this.state.count - 1;
           this.setState({
               count: newCount >= 0 ? newCount : 0
           })
+          if (newCount === 0) {
+             this.setState({countdownStatus: 'stopped'});
+          }
         } , 1000);
     },
     handleSetCountdown: function (seconds){
